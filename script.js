abb11ad050a804bc9a3961ed998f8716bc0832c2
luzmmerlim <luziamerlim.1309@gmail.com>
2022-02-14 16:00:26 -0300

    exercicio for-while

---
+++ script.js
@@ -0,0 +1,20 @@
+//Exercício 1
+for (let i=1; i <= 10; i++){
+    console.log(i)
+}
+  
+//Exercício 2
+let i = 1;
+while(i <=10){
+    console.log(i);
+    i++;
+}
+//Exercício 3
+  for( let i = 1; i <= 100; i += 2){
+     console.log(i)
+}
+  
+//Exercício 4
+  for( let i = 0; i <= 100; i += 2){
+     console.log(i)
+}
